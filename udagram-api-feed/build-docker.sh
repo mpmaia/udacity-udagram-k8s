7a701328c0916b8d0f9bc2cbd2953370823e202b
Mauricio Maia <mauricio@mpmaia.com.br>
2021-05-24 20:49:09 -0300

    Breaks the backend api

---
+++ udagram-api-feed/build-docker.sh
@@ -0,0 +1,2 @@
+docker build . -t mpmaia/udagram-api-feed
+# docker run --env-file ../docker_env mpmaia/udagram-api-feed
---
+++ udagram-api-users/build-docker.sh
@@ -0,0 +1,2 @@
+docker build . -t mpmaia/udagram-api-users
+# docker run --env-file ../docker_env mpmaia/udagram-api-users
