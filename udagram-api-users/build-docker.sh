6cbfe8a54cb459a391c7ab9fea3e2e4d0e6d0c25
Mauricio Maia <mauricio@mpmaia.com.br>
2021-05-25 11:20:38 -0300

    Fixes udagram-api-users build

--- udagram-api-users/build-docker.sh
+++ udagram-api-users/build-docker.sh
@@ -1,2 +1,2 @@
 docker build . -t mpmaia/udagram-api-users
-# docker run --env-file ../docker_env mpmaia/udagram-api-users
+# docker run --env-file ../docker_env -p 8080:8080 mpmaia/udagram-api-users
